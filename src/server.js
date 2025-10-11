2a415f911b785564c4b89c116b1c239e9c10dce9
Vladyslav Tykhyi <vladleonidovich01@gmail.com>
2025-10-11 15:20:54 +0200

    validation/filtration

--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import helmet from 'helmet';
 import { logger } from './middleware/logger.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import notesRoutes from './routes/notesRoutes.js';
+
+import router from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -16,9 +18,11 @@ app.use(express.json({ limit: 100 }));
 app.use(cors());
 app.use(helmet());
 
-app.use('/notes', notesRoutes);
+app.use('/notes', router);
 
 app.use(notFoundHandler);
+
+app.use(errors());
 
 app.use(errorHandler);
 
