deeb7d2e32fc1768ef20e88d241b554da2e5ff8c
Vladyslav Tykhyi <vladleonidovich01@gmail.com>
2025-10-25 08:35:50 +0200

    change middleware orde

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -116,13 +116,12 @@ export const requestResetEmail = async (req, res, next) => {
     { expiresIn: '15m' },
   );
 
-  // 1. Формуємо шлях до шаблона
   const templatePath = path.resolve('src/templates/reset-password-email.html');
-  // 2. Читаємо шаблон
+
   const templateSource = await fs.readFile(templatePath, 'utf-8');
-  // 3. Готуємо шаблон до заповнення
+
   const template = handlebars.compile(templateSource);
-  // 4. Формуємо із шаблона HTML документ з динамічними даними
+
   const html = template({
     name: user.username,
     link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
@@ -133,7 +132,6 @@ export const requestResetEmail = async (req, res, next) => {
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
-      // 5. Передаємо HTML у функцію надписання пошти
       html,
     });
   } catch {
@@ -155,27 +153,21 @@ export const resetPassword = async (req, res, next) => {
   try {
     payload = jwt.verify(token, process.env.JWT_SECRET);
   } catch {
-    // Повертаємо помилку якщо проблема при декодуванні
     next(createHttpError(401, 'Invalid or expired token'));
     return;
   }
 
-  // 2. Шукаємо користувача
   const user = await User.findOne({ _id: payload.sub, email: payload.email });
   if (!user) {
     next(createHttpError(404, 'User not found'));
     return;
   }
 
-  // 3. Якщо користувач існує
-  // створюємо новий пароль і оновлюємо користувача
   const hashedPassword = await bcrypt.hash(password, 10);
   await User.updateOne({ _id: user._id }, { password: hashedPassword });
 
-  // 4. Інвалідовуємо всі можливі попередні сесії користувача
   await Session.deleteMany({ userId: user._id });
 
-  // 5. Повертаємо успішну відповідь
   res.status(200).json({
     message: 'Password reset successfully. Please log in again.',
   });
